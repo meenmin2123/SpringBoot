3be5fa123375d81b595fc97b35c92d22811b433f
meenmin2123 <80881753+meenmin2123@users.noreply.github.com>
2024-09-05 15:26:58 +0900

    Delete all files from master branch

--- article/src/main/java/com/ss/article/repository/UserResitory.java
+++
@@ -1,9 +0,0 @@
-package com.ss.article.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ss.article.entity.User;
-
-public interface UserResitory extends JpaRepository<User, String>{
-
-}
--- restful/src/main/java/com/ss/restful/repository/ArticleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ss.restful.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ss.restful.entity.Article;
-
-public interface ArticleRepository extends JpaRepository<Article, Long>{
-
-}
